141dad916dbeae7c8ac1379b18826d03b8c55c3b
sajidiqbalbhatti <sajidiqbal.bk8888@gmail.com>
2026-03-06 21:27:36 +0500

    update the code

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -5,10 +5,9 @@ import dj_database_url
 # Base directory
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # SECURITY
-SECRET_KEY = 'django-insecure-_d55@*%9+^7g6@ere5_!$ug2dm^=kkhqmosb(&m(b=be%_y1ro'
-DEBUG = True
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-_d55@*%9+^7g6@ere5_!$ug2dm^=kkhqmosb(&m(b=be%_y1ro')
+DEBUG = False  # Production ke liye False
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
@@ -16,8 +15,9 @@ ALLOWED_HOSTS = [
     "bhatti.pythonanywhere.com",
     "django-job-portal-iifp.onrender.com",
     "django-job-portal-dmxk.onrender.com",
+    "careerlyjobs.com",
+    "www.careerlyjobs.com",
 ]
-
 
 # Applications
 INSTALLED_APPS = [
@@ -35,7 +35,6 @@ INSTALLED_APPS = [
     'core',
 ]
 
-
 # Middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -49,9 +48,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-
 ROOT_URLCONF = 'jobportal.urls'
-
 
 # Templates
 TEMPLATES = [
@@ -69,19 +66,15 @@ TEMPLATES = [
     },
 ]
 
-
 WSGI_APPLICATION = 'jobportal.wsgi.application'
-
 
 # Custom user model
 AUTH_USER_MODEL = 'users.User'
-
 
 # Database (Render PostgreSQL)
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
@@ -91,14 +84,12 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-
 # Internationalization
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 USE_TZ = True
-
 
 # Static files
 STATIC_URL = '/static/'
@@ -111,11 +102,20 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Security settings for production
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
 
 # Default primary key field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
